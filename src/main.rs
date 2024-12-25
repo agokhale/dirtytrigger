2f25f3d843a5b96aff99b739dc6a8252b7499b69
ash <ash@aeria.net>
2024-12-24 19:10:11 -0500

    astonished that this compiles - -but rust so it must be aight

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate yaml_rust;
+
+use std::env;
+use std::io;
+use std::io::Read;
+
+fn usage() {
+    println!("blharg! {:#?}", env::args());
+}
+
+fn dirty() {
+    let mut ringbuf: [u8;32] = [0xaf; 32];
+    let  rbp:u8 =  31; 
+    let mut stdin = io::stdin();
+    while true{
+        let mut rdsiz: usize = 0;
+        rdsiz = stdin.read(&mut ringbuf).unwrap();
+        println!("got{:?}>{:?}",rdsiz,ringbuf);
+    }
+
+}
 fn main() {
-    println!("Hello, world!");
+    // 
+    let argcl: Vec<String>  = env::args().collect();
+    println!("lenarg{:?}", argcl.len());
+    if (argcl.len() < 2 ){
+        usage();
+    }
+    dirty(); 
 }
